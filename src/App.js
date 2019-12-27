ef380d4c96605f511b364e397ff5ce3745efb8cc
George Arnold <george.arnold.88@gmail.com>
2019-12-27 16:48:01 -0500

    uploaded correct version from Thinkful

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from './SearchBar/SearchBar';
-import FilterableList from './FilterableList/FilterableList';
 
-class App extends Component () {
+class App extends Component {
   render() {
     return (
-      <main className='App'>
-      <SearchBar/>
-      <FilterableList files = {this.props.files}/>
-      </main>
+      <div className="App">
+        <h1>YOUR APPLICATION NAME!</h1>  
+      </div>
     );
   }
 }
